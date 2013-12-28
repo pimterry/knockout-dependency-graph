f03676c301a3ba5cef06d6a6098d5ac6736af679
Tim Perry <pimterry@gmail.com>
2013-12-28 04:15:34 +0000

    Add graph node ordering test

--- test/functional-test.js
+++ test/functional-test.js
@@ -26,6 +26,19 @@ define([
                 expect(graph.nodes[0].subscribable).toEqual(computed);
             });
 
+            it('Should track nodes in order of creation time', function () {
+                var graph = new KoDependencyGraph(ko);
+
+                var observable1 = ko.observable();
+                var observable2 = ko.observable();
+                var observable3 = ko.observable();
+
+                expect(graph.nodes.length).toEqual(3);
+                expect(graph.nodes[0].subscribable).toEqual(observable1);
+                expect(graph.nodes[1].subscribable).toEqual(observable2);
+                expect(graph.nodes[2].subscribable).toEqual(observable3);
+            });
+
             xit('Should track dependencies between observables', function () {
                 var graph = new KoDependencyGraph(ko);
 
