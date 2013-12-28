5498cd2527bc40a762f1c3f82873b4225c9ebc10
Tim Perry <pimterry@gmail.com>
2013-12-28 04:10:21 +0000

    Working KO observable tracking implementation

--- test/functional-test.js
+++ test/functional-test.js
@@ -3,12 +3,40 @@
 define([
     'knockout3',
     'lib/knockout-dependency-graph'
-], function(ko, koGrapher) {
+], function(ko, KoDependencyGraph) {
     describe('Functional tests', function () {
         describe('Basic usage', function () {
-           it('Should track dependencies between observables', function () {
+            it('Should track observable creation', function () {
+                var graph = new KoDependencyGraph(ko);
 
-           });
+                var observable = ko.observable();
+
+                expect(graph.nodes.length).toEqual(1);
+                expect(graph.nodes[0].subscribable).toEqual(observable);
+            });
+
+            it('Should track computed creation', function () {
+                var graph = new KoDependencyGraph(ko);
+
+                var computed = ko.computed(function () {
+                    return 1;
+                });
+
+                expect(graph.nodes.length).toEqual(1);
+                expect(graph.nodes[0].subscribable).toEqual(computed);
+            });
+
+            xit('Should track dependencies between observables', function () {
+                var graph = new KoDependencyGraph(ko);
+
+                var observable = ko.observable();
+                var computed = ko.computed(function () {
+                    return observable();
+                });
+
+                expect(graph.nodes[0].subscribers).toEqual([computed]);
+                expect(graph.nodes[1].subscriptions).toEqual([observable]);
+            });
         });
     });
 });
